91f245cedf7cfbf162f37f63cfb50cae235fd704
IwanVonNeumann <grabageheap@inbox.lv>
2017-10-27 09:23:22 +0300

    Row combinations listing added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from log_utils import get_true_inputs
-from matrix_coverage import get_implicant_matrix, print_implicant_matrix
+from matrix_coverage import get_implicant_matrix, print_implicant_matrix, list_all_combinations
 from simple_implicants import list_simple_implicants
 
 func_output_code = [1, 0, 1, 0, 1, 1, 0, 0]
@@ -14,3 +14,5 @@ print(simple_implicants)
 implicant_matrix = get_implicant_matrix(simple_implicants, true_inputs)
 
 print_implicant_matrix(simple_implicants, true_inputs, implicant_matrix)
+
+print(list_all_combinations(simple_implicants))
--- matrix_coverage.py
+++ matrix_coverage.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def get_implicant_matrix(simple_implicants, true_inputs):
     return [[1 if covers(impl, inp) else 0 for inp in true_inputs] for impl in simple_implicants]
 
@@ -16,3 +19,12 @@ def print_implicant_matrix(simple_implicants, true_inputs, implicant_matrix):
 
 def int_matrix_to_str_matrix(matrix):
     return [[str(x) for x in row] for row in matrix]
+
+
+def list_all_combinations(items):
+    all_combinations = [list(itertools.combinations(items, i + 1)) for i in range(len(items))]
+    return flat_list(all_combinations)
+
+
+def flat_list(list_of_lists):
+    return [item for sublist in list_of_lists for item in sublist]
