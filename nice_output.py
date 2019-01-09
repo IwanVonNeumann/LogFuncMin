4033f6b722ea827d1e1c4faae2f904323a8f07cf
IwanVonNeumann <grabageheap@inbox.lv>
2019-01-09 13:00:55 +0200

    logging improved

--- nice_output.py
+++ nice_output.py
@@ -9,12 +9,12 @@ def solve_with_nice_output(func_output_code):
     print()
 
     true_inputs = get_true_inputs(func_output_code)
-    print("True input sets:")
+    print("True input sets ({}):".format(len(true_inputs)))
     print(true_inputs)
     print()
 
     simple_implicants = list_simple_implicants(true_inputs)
-    print("Simple implicants:")
+    print("Simple implicants ({}):".format(len(simple_implicants)))
     print(simple_implicants)
     print()
 
