430b0edadbcf16c6ce7b57d33c4074d79b5ef703
IwanVonNeumann <grabageheap@inbox.lv>
2019-02-08 17:29:58 +0200

    logging improved

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from log_utils import generate_func_output
 from nice_output import solve_with_nice_output
 
-func_output_code = [1, 0, 1, 0, 1, 1, 0, 1]
+# func_output_code = [1, 0, 1, 0, 1, 1, 0, 1]
+func_output_code = [1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1]
 # func_output_code = generate_func_output(4, threshold=0.8)
 
-solve_with_nice_output(func_output_code)
+solve_with_nice_output(func_output_code, log=True)
--- nice_output.py
+++ nice_output.py
@@ -3,26 +3,30 @@ from matrix_coverage import get_implicant_matrix, print_implicant_matrix, solve_
 from simple_implicants import list_simple_implicants
 
 
-def solve_with_nice_output(func_output_code):
+def solve_with_nice_output(func_output_code, log=False):
     print("Function outputs code:")
     print(func_output_code)
     print()
 
     true_inputs = get_true_inputs(func_output_code)
-    print("True input sets ({}):".format(len(true_inputs)))
-    print(true_inputs)
-    print()
+    if log:
+        print("True input sets ({}):".format(len(true_inputs)))
+        print(true_inputs)
+        print()
 
     simple_implicants = list_simple_implicants(true_inputs)
-    print("Simple implicants ({}):".format(len(simple_implicants)))
-    print(simple_implicants)
-    print()
+    if log:
+        print("Simple implicants ({}):".format(len(simple_implicants)))
+        print(simple_implicants)
+        print()
 
-    print("Implicant matrix:")
     implicant_matrix = get_implicant_matrix(simple_implicants, true_inputs)
-    print_implicant_matrix(simple_implicants, true_inputs, implicant_matrix)
-    print()
+    if log:
+        print("Implicant matrix:")
+        print_implicant_matrix(simple_implicants, true_inputs, implicant_matrix)
+        print()
 
-    solve_by_min_length(simple_implicants, true_inputs, log=True)
+    min_length_coverages = solve_by_min_length(simple_implicants, true_inputs, log=log)
+    min_rank_coverages = solve_by_min_rank(simple_implicants, true_inputs, log=log)
 
-    solve_by_min_rank(simple_implicants, true_inputs, log=True)
+    return min_length_coverages, min_rank_coverages
